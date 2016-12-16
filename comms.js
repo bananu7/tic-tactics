7043b2ecd950cf69d8d618e6dfa11d86dbbae1eb
bananu7 <bananu7@o2.pl>
2016-12-16 23:28:50 +0100

    Changed the welcome page to be just lobby

--- comms.js
+++ comms.js
@@ -1,17 +1,22 @@
 "use strict";
 
 function initComms() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBmhRuR6w6d8W1UotiIaTGQ7n3yPlSIwYw",
+        authDomain: "tic-tactics-7a476.firebaseapp.com",
+        databaseURL: "https://tic-tactics-7a476.firebaseio.com",
+        storageBucket: "tic-tactics-7a476.appspot.com",
+        messagingSenderId: "934965263370"
+    };
+    firebase.initializeApp(config);
+
     console.log('init');
 
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {        
             console.log('signed in as ' + user.uid);
-
-            firebase.database().ref('games').on("child_added", function(data) {
-                $("#games").append($("<div></div>").text(data.val()));
-            }, function (errorObject) {
-                console.log("The read failed: " + errorObject.code);
-            });
+            attachUpdateHooks();
         } else {
             firebase.auth().signInWithEmailAndPassword('example@example.com', 'example').catch(function(error) {
                 // Handle Errors here.
@@ -23,16 +28,21 @@ function initComms() {
     });
 }
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBmhRuR6w6d8W1UotiIaTGQ7n3yPlSIwYw",
-    authDomain: "tic-tactics-7a476.firebaseapp.com",
-    databaseURL: "https://tic-tactics-7a476.firebaseio.com",
-    storageBucket: "tic-tactics-7a476.appspot.com",
-    messagingSenderId: "934965263370"
-};
-firebase.initializeApp(config);
+function attachUpdateHooks() {
+    firebase.database().ref('games').on("child_added", function(data) {
+        $("#games").append(newGameDiv(data));
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+}
 
+function newGameDiv(data) {
+    var elem = $("<div></div>");    
+    elem.addClass("game");
+    elem.append($("<h1></h1>").text(data.key));
+    elem.append($("<a>join!</a>").attr("href", "/draw.html&id=" + data.val));
+    return elem;
+}
 
 
 // Actual communication logic
