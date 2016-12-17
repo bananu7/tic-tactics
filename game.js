ba63c121bc6c7536e0f6691a128711bd03b4ee8c
bananu7 <bananu7@o2.pl>
2016-12-17 13:40:57 +0100

    Added more functionality to endTurn button

--- game.js
+++ game.js
@@ -8,5 +8,11 @@ function lockIn() {
 }
 
 function endTurn() {
+    // this will become enabled again once your turn comes
+    $("#end-turn-button").attr("disabled", true);
     connection.endTurn();
 }
+
+connection.onYourTurn(function() {
+    $("#end-turn-button").attr("disabled", false);
+});
